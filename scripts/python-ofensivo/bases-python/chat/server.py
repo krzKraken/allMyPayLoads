496323810360067e162bcfc863f61fdd78220e98
krzkraken <cristhian.mirandas@gmail.com>
2024-05-22 12:05:06 -0500

    server chat

--- scripts/python-ofensivo/bases-python/chat/server.py
+++ scripts/python-ofensivo/bases-python/chat/server.py
@@ -1,26 +1,33 @@
-import socket 
+import socket
 import threading
 
-def server_program():
-    HOST= 'localhost'
-    PORT= 12345
-    #Create socket 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #Settings socket (reutilize ports)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen()
-    print(f'[+] El servidor esta a la escucha del servidor por el puerto {PORT}')
 
+def server_program():
+
+    HOST = "localhost"
+    PORT = 12345
+
+    # NOTE: Create server socket IPV4 family
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # NOTE: SetSocketOpt -> reuse ports
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # NOTE: Socket en host y port
+    server_socket.bind((HOST, PORT))
+    # NOTE: Socket en escucha de n conexiones
+    server_socket.listen()
+
+    print(f"[+] Server runing on {HOST}:{PORT} waiting for conections...")
+
+    # INFO: What's server do on any confirmed conection
     clients = []
-    usernames={}
+    usernames = {}
 
     while True:
-        client_socket, address = server_socket.accept()
+
+        client_socket, client_address = server_socket.accept()
         clients.append(client_socket)
-        print(f'[+] Se ha conectado un cliente {address}')
+        print(f"[+] Client has connected: \n\t { client_socket } \n\t {client_address}")
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     server_program()
-
